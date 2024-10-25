4362c29bb9db7577cd83f1854055b154c51f0f20
Josh Mu <hello@joshmu.dev>
2024-10-25 12:06:01 +0800

    build: release

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [Commitizen](https://commitizen-tools.github.io/commitizen/) for commit guidelines.
---
+++ realease.config.cjs
@@ -0,0 +1,45 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'docs', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'build', release: 'patch' },
+          { type: 'ci', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+        ],
+      },
+    ],
+    ['@semantic-release/release-notes-generator'],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle:
+          '# Changelog\n\nAll notable changes to this project will be documented in this file. See [Commitizen](https://commitizen-tools.github.io/commitizen/) for commit guidelines.\n\n',
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
