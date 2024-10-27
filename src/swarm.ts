78b7526091fe9f48a3b153fb4aa5d1cedcab3f25
Josh Mu <hello@joshmu.dev>
2024-10-27 14:18:21 +0800

    docs(swarm): update comments

--- src/swarm.ts
+++ src/swarm.ts
@@ -43,6 +43,10 @@ export class Swarm extends EventEmitter {
     }
   }
 
+  /**
+   * Normalize the result of the agent function
+   * Takes in to account the possibility of the result being an agent
+   */
   private handleFunctionResult(result: any, debug: boolean): Result {
     if (result && typeof result === 'object' && 'value' in result) {
       return result;
@@ -62,25 +66,41 @@ export class Swarm extends EventEmitter {
     }
   }
 
+  /**
+   * Handle the list of tool call responses from the LLM
+   */
   private handleToolCalls(
     tool_calls: ChatCompletionMessageToolCall[],
     functions: AgentFunction[],
     context_variables: Record<string, any>,
     debug: boolean,
   ): Response {
+    /**
+     * Create the function tool registry
+     */
     const function_map: Record<string, AgentFunction> = {};
     functions.forEach((func) => {
       function_map[func.name] = func;
     });
 
+    /**
+     * Initialize the swarm response output
+     */
     const partialResponse = createSwarmResult({
       messages: [],
       agent: undefined,
       context_variables: {},
     });
 
+    /**
+     * Iterate over the tools which have been called
+     */
     tool_calls.forEach((tool_call) => {
       const name = tool_call.function.name;
+      /**
+       * Check if the tool is registered
+       * And if not, log an error and add it to the response
+       */
       if (!(name in function_map)) {
         logger(debug, `Tool ${name} not found in function map.`);
         partialResponse.messages.push({
@@ -92,6 +112,9 @@ export class Swarm extends EventEmitter {
         return;
       }
 
+      /**
+       * Retrive the function tool arguments
+       */
       const args = JSON.parse(tool_call.function.arguments);
       logger(
         debug,
@@ -99,14 +122,20 @@ export class Swarm extends EventEmitter {
         JSON.stringify(args),
       );
 
+      /**
+       * Provide the context variables to the function if required
+       */
       const func = function_map[name];
-      // Pass context_variables to agent functions if required
       const hasFunc = func.func.length;
       const hasContextVars = (func as Record<string, any>).hasOwnProperty(
         CTX_VARS_NAME,
       );
       if (hasFunc && hasContextVars) args[CTX_VARS_NAME] = context_variables;
 
+      /**
+       * Validate the arguments
+       * By ensuring they match the function descriptor
+       */
       let validatedArgs: any;
       try {
         validatedArgs = validateAgentFuncArgs(args, func.descriptor);
@@ -130,21 +159,36 @@ export class Swarm extends EventEmitter {
         JSON.stringify(validatedArgs),
       );
 
-      // Invoke the function with the validated arguments
+      /**
+       * Invoke the function with the validated arguments
+       */
       const raw_result = func.func(validatedArgs);
       logger(debug, 'Raw result:', raw_result);
 
+      /**
+       * Normalize the result of the agent function
+       * Takes in to account the possibility of the result being an agent
+       */
       const result: Result = this.handleFunctionResult(raw_result, debug);
+      /**
+       * Add the result to the response
+       */
       partialResponse.messages.push({
         role: 'tool',
         tool_call_id: tool_call.id,
         tool_name: name,
         content: result.value,
       });
+      /**
+       * Update the context variables
+       */
       partialResponse.context_variables = {
         ...partialResponse.context_variables,
         ...result.context_variables,
       };
+      /**
+       * If the result is an agent, switch to it
+       */
       if (result.agent) {
         partialResponse.agent = result.agent;
         this.emit('agentSwitch', result.agent);
@@ -159,6 +203,9 @@ export class Swarm extends EventEmitter {
     return partialResponse;
   }
 
+  /**
+   * Run the swarm by making a single LLM request which is streamed
+   */
   async *runAndStream(options: SwarmRunOptions<true>): AsyncIterable<any> {
     const {
       agent,
@@ -260,6 +307,9 @@ export class Swarm extends EventEmitter {
     };
   }
 
+  /**
+   * Run the swarm by making a single LLM request which is NOT streamed
+   */
   async run<TStream extends boolean = false>(
     options: SwarmRunOptions<TStream>,
   ): Promise<TStream extends true ? AsyncIterable<any> : Response> {
@@ -274,6 +324,9 @@ export class Swarm extends EventEmitter {
       execute_tools = true,
     } = options;
 
+    /**
+     * Handle a streamed response by delegating to the runAndStream method
+     */
     if (stream) {
       return this.runAndStream({
         agent,
@@ -286,13 +339,21 @@ export class Swarm extends EventEmitter {
       }) as TStream extends true ? AsyncIterable<any> : never;
     }
 
+    /**
+     * Initialize
+     */
     let active_agent = agent;
     let ctx_vars = structuredClone(context_variables);
     const history = structuredClone(messages);
     const initialMessageLength = history.length;
 
+    /**
+     * Iterate
+     */
     while (history.length - initialMessageLength < max_turns && active_agent) {
-      // Update the getChatCompletion call
+      /**
+       * Make the LLM request
+       */
       const completion: ChatCompletion = await getChatCompletion(
         this.client,
         active_agent,
@@ -303,29 +364,50 @@ export class Swarm extends EventEmitter {
         debug,
       );
 
+      /**
+       * Update the history
+       */
       const messageData = completion.choices[0].message;
       logger(debug, 'Received completion:', messageData);
       const message: any = { ...messageData, sender: active_agent.name };
-      history.push(message); // Adjust as needed
+      history.push(message);
 
+      /**
+       * If there are no tool calls, end the turn
+       */
       if (!message.tool_calls || !execute_tools) {
         logger(debug, 'Ending turn.');
         break;
       }
 
-      // Handle function calls, updating context_variables and switching agents
+      /**
+       * Handle function calls, updating context_variables and switching agents
+       */
       const partial_response = this.handleToolCalls(
         message.tool_calls,
         active_agent.functions,
         ctx_vars,
         debug,
       );
+      /**
+       * Add to history
+       */
       history.push(...partial_response.messages);
+      /**
+       * Update context
+       */
       ctx_vars = { ...ctx_vars, ...partial_response.context_variables };
+      /**
+       * If the result is an agent, switch to it
+       */
       if (partial_response.agent) {
         active_agent = partial_response.agent;
       }
     }
+    /**
+     * Return the result
+     * However only the messages associated to this run
+     */
     const newMessages = history.slice(initialMessageLength);
     return createSwarmResult({
       messages: newMessages,
